21c4f980b5fc5b6abae8cd0d2a60392bdf8ffdcb
Arun <arunkumard0606@gmail.com>
2025-05-11 15:30:39 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+  });
+
+app.get('/pickup', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'pickup.html'));
+  });
+  app.get('/confirmation', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'Confirmation.html'));
+  });
+  app.get('/how', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'process1.html'));
+  });
+  app.get('/rewards', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'rewards.html'));
+  });
+  
+
+  app.get('/confirmation', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+  });
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
